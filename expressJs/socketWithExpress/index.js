83e1b9fa368adfdc9a94495e36b21449bee85c37
Shahed Mahamud Lemon <lemonkhaan1@gmail.com>
2022-02-01 00:48:27 +0600

    socket configured with client and server side

--- expressJs/socketWithExpress/index.js
+++ expressJs/socketWithExpress/index.js
@@ -4,9 +4,17 @@ const http=require('http');
 const expresServer=http.createServer(app);
 const {Server}= require("socket.io");
 const io=new Server(expresServer);
+
+io.on("connection",function (socket){
+    console.log("New user connected");
+
+    socket.on("disconnect",function (){
+        console.log("User disconnected")
+    })
+})
 app.get("/",(req,res)=>{
     res.sendFile(__dirname+"/index.html");
 })
-server.listen(8000,()=>{
+expresServer.listen(8000,()=>{
     console.log("Server run success")
 })
